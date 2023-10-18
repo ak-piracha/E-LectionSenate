bcbb50fc6c4b251d90c571d5559e2a06e2b871ca
Jay University of Adelaide <jay.chauhan@student.adelaide.edu.au>
2023-10-18 21:14:06 +1030

    feat: db migrations for base archi added

---
+++ database/migrations/2023_10_18_100155_create_votes_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('votes', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('voters');
+            $table->foreignId('election_id')->constrained('elections');
+            $table->foreignId('type_id')->constrained(''); #name of the typeTable
+            $table->foreignId('candidate_id')->constrained(''); #name of the candidate
+            $table->integer('priority');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('votes');
+    }
+};
---
+++ database/migrations/2023_10_18_102142_create_aec_commissioners_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('aec_commissioners', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('email')->unique();
+            $table->date('start_date');
+            $table->string('phone_number'); 
+            $table->date('last_date')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('aec_commissioners');
+    }
+};
