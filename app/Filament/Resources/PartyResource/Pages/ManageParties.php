eb6929440bd0f895f76af8479faa240a7efad1f2
Jay University of Adelaide <jay.chauhan@student.adelaide.edu.au>
2023-11-14 14:02:57 +1030

    feat: set up party resource

---
+++ app/Filament/Resources/PartyResource/Pages/ManageParties.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Filament\Resources\PartyResource\Pages;
+
+use App\Filament\Resources\PartyResource;
+use Filament\Actions;
+use Filament\Resources\Pages\ManageRecords;
+
+class ManageParties extends ManageRecords
+{
+    protected static string $resource = PartyResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\CreateAction::make(),
+        ];
+    }
+}
