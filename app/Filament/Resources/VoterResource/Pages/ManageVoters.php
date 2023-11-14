18cb7d2e97a44a032f9af2e9db42da5147eaa123
Jay University of Adelaide <jay.chauhan@student.adelaide.edu.au>
2023-11-15 00:15:10 +1030

    feat:set up authorization

--- app/Filament/Resources/VoterResource/Pages/ManageVoters.php
+++ app/Filament/Resources/VoterResource/Pages/ManageVoters.php
@@ -13,7 +13,7 @@ class ManageVoters extends ManageRecords
     protected function getHeaderActions(): array
     {
         return [
-            // Actions\CreateAction::make(),
+            Actions\CreateAction::make(),
         ];
     }
 }
